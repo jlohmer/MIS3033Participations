aea1bac6521db22ecad36cd78820f4ff55d048cf
lohm5697 <jason.c.lohmer@ou.edu>
2020-01-23 13:10:42 -0600

    did stuff

---
+++ InClassExamples/Review_Loops_Collections_Classes/Program.cs
@@ -0,0 +1,84 @@
+﻿//Jason Lohmer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Loops_Collections_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parallel Arrays
+            int[] studentIds = new int[3];
+            string[] studentFirstNames = new string[3];
+            string[] studentLastNames = { "Harris", "Ackerman", "Johnson" };
+            studentIds[0] = 1;
+            studentIds[1] = 2;
+            studentIds[2] = 3;
+            studentFirstNames[0] = "Katie";
+            studentFirstNames[1] = "Adam";
+            studentFirstNames[2] = "Harry";
+
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                Console.WriteLine($"{studentLastNames[i]}, {studentFirstNames[i]}, [{studentIds[i]}]");
+            }
+
+            //Dictionary - key/value pairs
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(1, "Katie Harris");
+            students.Add(2, "Adam Ackerman");
+            students.Add(3, "Jason Lohmer");
+
+            foreach (var key in students.Keys)
+            {
+                Console.WriteLine($"{students[key]} [{key}]");
+            }
+
+            List<Student> studs = new List<Student>();
+
+            Student katie = new Student();
+            katie.FirstName = "Katie";
+            katie.LastName = "Harris";
+            katie.ID = 1;
+            katie.Birthdate = Convert.ToDateTime("3/11/1998");
+
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                Student s = new Student();
+                s.FirstName = studentFirstNames[i];
+                s.LastName = studentLastNames[i];
+                s.ID = studentIds[i];
+
+                studs.Add(s);
+            }
+
+            foreach (var student in studs)
+            {
+                Console.WriteLine($"{student.LastName}, {student.FirstName}, [{student.ID}], was born on {student.Birthdate.ToLongDateString()}");
+                bool isUpper = true;
+                string crazyFirstName = "";
+                foreach (var letter in student.FirstName)
+                {
+                    if (isUpper == true)
+                    {
+                        crazyFirstName += letter.ToString().ToUpper();
+                        isUpper = false;
+                    }
+                    else
+                    {
+                        crazyFirstName += letter.ToString().ToLower();
+                        isUpper = true;
+                    }
+                }
+                Console.WriteLine($"Crazy firstname is {crazyFirstName}");
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InClassExamples/Review_Loops_Collections_Classes/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Loops_Collections_Classes
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        private int LuckyNumber;
+
+        public Student()
+        {
+            ID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Birthdate = DateTime.Now;
+            LuckyNumber = 0;
+        }
+
+        public Student(int id, int luckyNumber)
+        {
+            ID = id;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Birthdate = DateTime.Now;
+            LuckyNumber = luckyNumber;
+        }
+
+        public string BirthdateDayOfWeek()
+        {
+            return "";
+        }
+    }
+}
